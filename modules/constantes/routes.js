70c8e3713423f9d52e56366108271598ce6b63ec
Suissa <jnascimento@gmail.com>
2016-05-11 01:22:00 -0300

    Iniciando

--- modules/constantes/routes.js
+++ modules/constantes/routes.js
@@ -1,64 +1,45 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const Grandezas = require('./grandezasFisicoQuimica');
 
 router.get('/', function(req, res, next) {
-  res.send('Busque por alguma CONSTANTE.');
-}); 
+  res.json(Grandezas.listaConstantes());
+});
+
 router.get('/:simbolo', function(req, res, next) {
-
-const BuscaConstantePorSimbolo = require('./grandezasFisicoQuimica');
-const buscaConstante = BuscaConstantePorSimbolo()
-
-  res.json(buscaConstante(req.params.simbolo));
+  res.json(Grandezas.procuraConstantePorSimbolo(req.params.simbolo));
 });
 
 
 router.get('/:simbolo/nome', function(req, res, next) {
-
-  const BuscaConstantePorSimbolo = require('./grandezasFisicoQuimica');
-  const buscaConstante = BuscaConstantePorSimbolo()
-  const constante = buscaConstante(req.params.simbolo)
+  const constante = Grandezas.procuraConstantePorSimbolo(req.params.simbolo);
   res.json(constante.nome);
 });
 
 router.get('/:simbolo/valor', function(req, res, next) {
-
-  const BuscaConstantePorSimbolo = require('./grandezasFisicoQuimica');
-  const buscaConstante = BuscaConstantePorSimbolo()
-  const constante = buscaConstante(req.params.simbolo)
+  const constante = Grandezas.procuraConstantePorSimbolo(req.params.simbolo);
   res.json(constante.valor);
 });
 
 router.get('/:simbolo/definicao', function(req, res, next) {
-
-  const BuscaConstantePorSimbolo = require('./grandezasFisicoQuimica');
-  const buscaConstante = BuscaConstantePorSimbolo()
-  const constante = buscaConstante(req.params.simbolo)
+  const constante = Grandezas.procuraConstantePorSimbolo(req.params.simbolo);
   res.json(constante.definicao);
 });
 
 router.get('/:simbolo/unidade', function(req, res, next) {
-
-  const BuscaConstantePorSimbolo = require('./grandezasFisicoQuimica');
-  const buscaConstante = BuscaConstantePorSimbolo()
-  const constante = buscaConstante(req.params.simbolo)
+  const constante = Grandezas.procuraConstantePorSimbolo(req.params.simbolo);
   res.json(constante.unidade);
 });
 
 router.get('/:simbolo/outrasunidades', function(req, res, next) {
-
-  const BuscaConstantePorSimbolo = require('./grandezasFisicoQuimica');
-  const buscaConstante = BuscaConstantePorSimbolo()
-  const constante = buscaConstante(req.params.simbolo)
+  const constante = Grandezas.procuraConstantePorSimbolo(req.params.simbolo);
   res.json(constante.valoresPorUnidade);
 });
 
-
 router.get('/:simbolo/unidade/:indice', function(req, res, next) {
-
-  const BuscaConstantePorSimbolo = require('./grandezasFisicoQuimica');
-  const buscaConstante = BuscaConstantePorSimbolo()
-  const constante = buscaConstante(req.params.simbolo)
+  const constante = Grandezas.procuraConstantePorSimbolo(req.params.simbolo);
   res.json(constante.unidade[req.params.indice]);
 });
 
